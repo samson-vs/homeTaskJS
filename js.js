2375ac46e75fcd60f61e307e6ed9fff4205f0aab
samson-vs <svvs.samson@gmail.com>
2023-05-02 17:11:44 +0300

    Initial commit

---
+++ js.js
@@ -0,0 +1,38 @@
+let h = 'Hello'
+let o = 'owu'
+let c = 'com'
+let u = 'ua'
+let num1 = 1
+let num2 = 10
+let num3 = -999
+let num4 = 123
+let num5 = 3.14
+let num6 = 2.7
+let num7 = 16
+let bul1 = true
+let bul2 = false
+
+console.log(h,o,c,u, num1, num2, num3, num4 , num5 , num6, num7, bul1, bul2)
+
+let firstName = 'Volodymyr'
+let middleName = 'Stepanovych'
+let lastName = 'Samson'
+
+let person = `${firstName} ${middleName} ${lastName}`
+console.log(person);
+
+let a = 100
+let b = '100'
+let c2 = true
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c2);
+
+let name2 = prompt(`your name?`)
+let surname = prompt(`your surname?`)
+let age = prompt(`your age?`)
+
+console.log(name2);
+console.log(surname);
+console.log(age);
